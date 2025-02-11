3fc0ea4b84e23f7fa632fc07284f9a0b1fe240ab
Neil Aditya Raman <neilr@andrew.cmu.edu>
2025-02-11 01:14:01 -0500

    Error Handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { SpotifyPlayer } from "./components/SpotifyPlayer";
 
 const queryClient = new QueryClient();
 
@@ -17,8 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/callback" element={<SpotifyPlayer />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/callback" element={<Navigate to="/" replace />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
